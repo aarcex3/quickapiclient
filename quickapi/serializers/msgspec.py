06c3b82eca248bb5d18ee593ba569b0f2a87f3b4
Agustin Arce <agustinarceprincich@gmail.com>
2024-09-25 10:46:39 -0400

    feat: add msgspec de-serializers

---
+++ quickapi/serializers/msgspec.py
@@ -0,0 +1,40 @@
+from quickapi.exceptions import DictSerializationError
+from quickapi.serializers.types import FromDictSerializableT
+
+try:
+    import msgspec
+except ImportError:
+    msgspec_installed = False
+else:
+    msgspec_installed = True
+
+
+class MsgspecSerializer:
+    """
+    Convert from dict to msgspec.Struct.
+    """
+
+    @classmethod
+    def can_apply(cls, klass: type[FromDictSerializableT]) -> bool:
+        return msgspec_installed and issubclass(klass, msgspec.Struct)
+
+    @classmethod
+    def from_dict(
+        cls, klass: type[FromDictSerializableT], values: dict
+    ) -> FromDictSerializableT:
+        try:
+            return klass(**values)
+        except msgspec.ValidationError as e:
+            raise DictSerializationError(expected_type=klass.__name__) from e
+
+
+class MsgspecDeserializer:
+    """Convert from msgspec.Struct to dict."""
+
+    @classmethod
+    def can_apply(cls, instance: "msgspec.Struct") -> bool:
+        return msgspec_installed and isinstance(instance, msgspec.Struct)
+
+    @classmethod
+    def to_dict(cls, instance: "msgspec.Struct") -> dict | None:
+        return msgspec.structs.asdict(instance)
